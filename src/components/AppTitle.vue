9228821c7341be66a1ebc6bd19e89238625d9780
FRA2721 <cimino.francesco36@gmail.com>
2022-11-07 15:28:13 +0100

    show title on screen

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<!-- script -->
+<script>
+export default{
+    data(){
+        return{
+            greeting: "Hello World!!! by Francesco Cimino"
+        }
+    }
+}
+</script>
+<!-- /script -->
+
+
+
+<!-- template-section -->
+<template>
+    <h1>{{ greeting }}</h1>
+</template>
+<!-- /template-section -->
+
+
+
+<!-- style -->
+<style scoped>
+</style>
+<!-- /style -->
